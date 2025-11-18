248ad6a7eb85f5501a4ed9721c1e261702df4ad3
Kristina <matweewakristina46@yandex.ru>
2025-11-18 20:25:03 +0300

    Add Flask blog API code2

---
+++ model/post.py
@@ -0,0 +1,5 @@
+class Post:
+    def __init__(self, body: str, author: str, id: int):
+        self.body = body
+        self.author = author
+        self.id = id
---
+++ model/user.py
@@ -0,0 +1,5 @@
+class User:
+
+    def __init__(self, username: str, age: int):
+        self.username = username
+        self.age = age
